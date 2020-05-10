85c9d8f9160a46f4613525b9ebf4b71c582982ba
Spazcool <douglas.wrightiii@gmail.com>
2020-05-09 22:41:41 -0400

    wrap highcharts in func to pass dynamic params

--- js/map.js
+++ js/map.js
@@ -1,81 +1,86 @@
+// todo pass different data points into this, not just death
+// todo slider for historical data
+// $().on('click', () =>  {
 
-// Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/us-population-density.json', function (data) {
-Highcharts.getJSON('https://covidtracking.com/api/v1/states/current.json', function (data) {
-// console.log(data)
-    // REMOVE TERRITORIES & RENAME STATE PROPERTY TO PLAY NICE WITH HIGHCHARTS MAP
-    data.splice(51, 5)
-    data.forEach((state) =>{
-        state.code = state.state;
-        state.value = state.death;
-        delete state.state;
-    })
-    // Instantiate the map
-    Highcharts.mapChart('map', {
-        chart: {
-            map: 'countries/us/us-all',
-            borderWidth: 1
-        },
+// })
 
-        title: {
-            text: 'US Covid-19 Deaths / State'
-        },
-
-        exporting: {
-            sourceWidth: 600,
-            sourceHeight: 500
-        },
-
-        legend: {
-            layout: 'horizontal',
-            borderWidth: 0,
-            backgroundColor: 'rgba(255,255,255,0.25)',
-            floating: true,
-            verticalAlign: 'top',
-            y: 25
-        },
-
-        mapNavigation: {
-            enabled: true
-        },
-
-        colorAxis: {
-            min: 1,
-            max: 10000,
-            type: 'logarithmic',
-            // minColor: '#AAA',
-            // maxColor: '#000',
-            minColor: '#EEEEFF',
-            maxColor: '#000022',
-            stops: [
-                [0, '#EFEFFF'],
-                [0.67, '#4444FF'],
-                [1, '#000022']
-                // [0, '#FFF'],
-                // [0.67, '#AAA'],
-                // [1, '#000']
-            ]
-        },
-
-        series: [{
-            animation: {
-                duration: 1000
+function loadMap(datum){
+    Highcharts.getJSON('https://covidtracking.com/api/v1/states/current.json', function (data) {
+        // console.log(data)
+        // REMOVE TERRITORIES & RENAME STATE PROPERTY TO PLAY NICE WITH HIGHCHARTS MAP
+        data.splice(51, 5)
+        data.forEach((state) =>{
+            state.code = state.state;
+            state.value = state[datum];
+            delete state.state;
+            delete state[datum];
+        })
+        // Instantiate the map
+        Highcharts.mapChart('map', {
+            chart: {
+                map: 'countries/us/us-all',
+                borderWidth: 2,
+    // COLOR
+                borderColor: '#FF0000'
             },
-            data: data,
-            joinBy: ['postal-code', 'code'],
-            dataLabels: {
-                enabled: true,
-                color: '#FFFFFF',
-                format: '{point.code}'
+    
+            title: {
+                text: `US Covid-19 ${datum} / State`
             },
-            name: 'Deaths',
-            tooltip: {
-                pointFormat: '{point.code}: {point.value}'
-            }
-        }]
+    
+            exporting: {
+                sourceWidth: 600,
+                sourceHeight: 500
+            },
+    
+            legend: {
+                layout: 'horizontal',
+                borderWidth: 0,
+                backgroundColor: 'rgba(255,255,255,0.25)',
+                floating: true,
+                verticalAlign: 'top',
+                y: 25
+            },
+    
+            mapNavigation: {
+                enabled: true
+            },
+    
+            colorAxis: {
+                min: 1,
+                max: 10000,
+                type: 'logarithmic',
+                stops: [
+    // COLOR
+                    [0, '#FFA07A'],
+                    [0.67, '#FF0000'],
+                    [1, '#800000']
+                ]
+            },
+    
+            series: [{
+                animation: {
+                    duration: 1000
+                },
+                data: data,
+                joinBy: ['postal-code', 'code'],
+                dataLabels: {
+                    enabled: true,
+                    color: '#FFFFFF',
+                    format: '{point.code}'
+                },
+                name: datum,
+                tooltip: {
+                    pointFormat: '{point.code}: {point.value}'
+                }
+            }]
+        });
     });
-});
+}
 
+loadMap('recovered')
 
+//Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/us-population-density.json', function (data) {
 // return await fetch(`https://covidtracking.com/api/v1/us/current.json`).then(response => response.json());
 // $( async function(){
 //     let getCovidStatsBy = async () => {
