261817a6447ce2788773739891175f6368c03f52
Spazcool <douglas.wrightiii@gmail.com>
2020-05-12 23:01:09 -0400

    clean up map.js, dynamically create frontend inputs

--- js/map.js
+++ js/map.js
@@ -1,90 +1,125 @@
 $(function(){
+    // SET INITIAL LOAD TO DEATH
     loadMap('death')
+    displayStatsOption()
 })
 
-$("input[type='radio']").on('click', function(){
-    loadMap(this.value)
-})
+const covidStatsList = {
+    "hospitalizedCumulative" : "Total Hospitalizations",
+    "hospitalizedCurrently" : "Current Hospitalizations",
+    "inIcuCumulative" : "Total ICUs",
+    "inIcuCurrently" : "Current ICUs",
+    "onVentilatorCumulative" : "Total Ventilators Used",
+    "onVentilatorCurrently" : "Current Ventilators In Use",
+    "recovered" : "Total Recoveries",
+    "death" : "Total Deaths",
+    "totalTestResults" : "Total Test Results",
+    "negative" : "Total Test Results (Negative)",
+    "positive" : "Total Test Results (Positive)",
+    "dataQualityGrade" : "Data Quality",
+};
 
-// todo slider for historical data
-// TODO FIND A MAP THAT INCLUDES THE TERRRITORIES
+function displayStatsOption() {
+    for (stat in covidStatsList) {
+      $(".control").append(
+          `<label class="radio">
+                <input type="radio" name="datum" value=${stat}>${covidStatsList[stat]}</label>
+           </label>`
+      );
+    }
+    $("input[type='radio']").on('click', function(){
+        loadMap(this.value)
+    })
+}
 
-function loadMap(datum){
-    Highcharts.getJSON('https://covidtracking.com/api/v1/states/current.json', function (data) {
-        // REMOVE TERRITORIES & RENAME STATE PROPERTY TO PLAY NICE WITH HIGHCHARTS MAP
-        data.splice(51, 5)
-        data.forEach((state) => {
-            state.code = state.state;
-            state.value = state[datum];
-            delete state.state;
-            delete state[datum];
-        })
-        // SET THE MIN/MAX RANGE FOR COLOR SCALE
-        let min = Math.min.apply(Math, data.map(function(state) { return state.value; })); 
-        let max = Math.max.apply(Math, data.map(function(state) { return state.value; }));
+function displayMap(data, datum){
+    // SET THE MIN/MAX RANGE FOR COLOR SCALE
+    let min = Math.min.apply(Math, data.map(function(state) { return state.value; })); 
+    let max = Math.max.apply(Math, data.map(function(state) { return state.value; }));
 
-        console.log(`${min} | ${max}`)
-        // Instantiate the map
-        Highcharts.mapChart('map', {
-            chart: {
-                map: 'countries/us/us-all',
-                borderWidth: 2,
-    // COLOR
-                borderColor: '#FF0000'
+    Highcharts.mapChart('map', {
+        chart: {
+            map: 'countries/us/us-all',
+            borderWidth: 2,
+            borderColor: '#FF0000'
+        },
+
+        title: {
+            text: `US Covid-19 ${covidStatsList[datum]} / State`
+        },
+
+        exporting: {
+            sourceWidth: 600,
+            sourceHeight: 500
+        },
+
+        legend: {
+            layout: 'horizontal',
+            borderWidth: 0,
+            backgroundColor: 'rgba(255,255,255,0.25)',
+            floating: true,
+            verticalAlign: 'top',
+            y: 25
+        },
+
+        mapNavigation: {
+            enabled: true
+        },
+
+        colorAxis: {
+            // MIN OF 0 KILLS THE MAP, DISPLAY AS 1
+            min: (min == 0 ? 1 : min),
+            max: max,
+            type: 'logarithmic',
+            stops: [
+                [0, '#FFA07A'],
+                [0.67, '#FF0000'],
+                [1, '#800000']
+            ]
+        },
+
+        series: [{
+            animation: {
+                duration: 1000
             },
-    
-            title: {
-// TODO DATUM NEEDS BE HUMAN READABLE
-                text: `US Covid-19 ${datum} / State`
+            data: data,
+            joinBy: ['postal-code', 'code'],
+            dataLabels: {
+                enabled: true,
+                color: '#FFFFFF',
+                format: '{point.code}'
             },
-    
-            exporting: {
-                sourceWidth: 600,
-                sourceHeight: 500
-            },
-    
-            legend: {
-                layout: 'horizontal',
-                borderWidth: 0,
-                backgroundColor: 'rgba(255,255,255,0.25)',
-                floating: true,
-                verticalAlign: 'top',
-                y: 25
-            },
-    
-            mapNavigation: {
-                enabled: true
-            },
-    
-            colorAxis: {
-                // MIN OF 0 KILLS THE MAP, DISPLAY AS 1
-                min: (min == 0 ? 1 : min),
-                max: max,
-                type: 'logarithmic',
-                stops: [
-    // COLOR
-                    [0, '#FFA07A'],
-                    [0.67, '#FF0000'],
-                    [1, '#800000']
-                ]
-            },
-    
-            series: [{
-                animation: {
-                    duration: 1000
-                },
-                data: data,
-                joinBy: ['postal-code', 'code'],
-                dataLabels: {
-                    enabled: true,
-                    color: '#FFFFFF',
-                    format: '{point.code}'
-                },
-                name: datum,
-                tooltip: {
-                    pointFormat: '{point.code}: {point.value}'
-                }
-            }]
-        });
+            name: datum,
+            tooltip: {
+                pointFormat: '{point.code}: {point.value}'
+            }
+        }]
     });
 }
+
+function loadMap(datum){
+    fetch('https://covidtracking.com/api/v1/states/current.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // REMOVE TERRITORIES & RENAME STATE PROPERTY TO PLAY NICE WITH HIGHCHARTS MAP
+            data.splice(51, 5)
+            data.forEach((state) => {
+                state.code = state.state;
+                state.value = state[datum];
+                delete state.state;
+                delete state[datum];
+            })
+            displayMap(data, datum)
+    });
+}
+
+$(".control-home").on("click", () => {
+    window.location.href = "index.html";
+});
+
+$(".navbar-burger").on("click", () => {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+});
